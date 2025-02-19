d38dbc02d795e8591d04a3542d714969f5d1e761
sohel200920 <scienceexploration20@gmail.com>
2025-02-19 19:46:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function changeGift() {
+  const giftDiv = document.getElementById("gift");
+  const gifts = {
+    1: { emoji: "🌹", text: "Thank You Mariya" },
+  };
+
+  const days = Object.keys(gifts);
+  const randomDay = days[Math.floor(Math.random() * days.length)];
+  const { emoji, text } = gifts[randomDay];
+
+  giftDiv.innerHTML = `<span>${emoji}<span><p>${text}</p>`;
+}
+
+document.getElementById("cube").addEventListener("click", function () {
+  this.classList.toggle("active");
+  if (!this.classList.contains("active")) {
+    changeGift();
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  changeGift();
+
+  const cube = document.getElementById("cube");
+  cube.classList.add("active");
+  setTimeout(() => {
+    cube.classList.remove("active");
+  }, 3000);
+});
